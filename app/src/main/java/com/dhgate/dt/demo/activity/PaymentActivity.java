30b24f9df2b54fd96b3bbbae073d833eb0acf0b0
FloraMeng <569597394@qq.com>
2018-04-11 17:42:27 +0800

    增加订单支付界面

--- app/src/main/java/com/dhgate/dt/demo/activity/PaymentActivity.java
+++ app/src/main/java/com/dhgate/dt/demo/activity/PaymentActivity.java
@@ -1,13 +1,26 @@
 package com.dhgate.dt.demo.activity;
 
+import android.widget.ImageView;
+
+import com.dhgate.dt.demo.R;
+
+import butterknife.Bind;
+import butterknife.OnClick;
+
 /**
  * Created by flora on 2018/4/11.
  */
 
 public class PaymentActivity extends BaseActivity {
+
+    @OnClick(R.id.confirm_payment_btn)
+    public void onConfirmClick() {
+
+    }
+
     @Override
     protected int getLayoutId() {
-        return 0;
+        return R.layout.activity_payment;
     }
 
     @Override
