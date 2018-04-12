c2f2665e89d584e978bb94face847306176f40ba
FloraMeng <569597394@qq.com>
2018-04-12 17:47:57 +0800

    增加我的买卖界面

--- app/src/main/java/com/dhgate/dt/demo/activity/MyAccountActivity.java
+++ app/src/main/java/com/dhgate/dt/demo/activity/MyAccountActivity.java
@@ -1,8 +1,12 @@
 package com.dhgate.dt.demo.activity;
 
 import android.content.Intent;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.dhgate.dt.demo.R;
+import com.dhgate.dt.demo.widget.slidingUpPanel.SlidingUpPanelLayout;
+import butterknife.Bind;
 import butterknife.OnClick;
 
 /**
@@ -10,6 +14,12 @@ import butterknife.OnClick;
  */
 
 public class MyAccountActivity extends BaseActivity {
+
+    @Bind(R.id.sliding_layout)
+    public SlidingUpPanelLayout sliding_layout;
+
+    @Bind(R.id.rel_complete_order)
+    RelativeLayout rel_complete_order;
 
     @OnClick(R.id.myaccount_img_2)
     public void onAccountClick() {
@@ -27,6 +37,36 @@ public class MyAccountActivity extends BaseActivity {
 
     }
 
+    @OnClick(R.id.iv_pay_back)
+    public void payBackClick() {
+        sliding_layout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+    }
+
+    @OnClick(R.id.my_bisness_2)
+    public void onBtn2Click() {
+
+    }
+
+    @OnClick(R.id.my_bisness_3)
+    public void onBtn3Click() {
+
+    }
+
+    @OnClick(R.id.my_bisness_4)
+    public void onBtn4Click() {
+
+    }
+
+    @OnClick(R.id.my_bisness_5)
+    public void onBtn5Click() {
+
+    }
+
+    @OnClick(R.id.my_bisness_6)
+    public void onBtn6Click() {
+
+    }
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_myaccount;
@@ -35,5 +75,35 @@ public class MyAccountActivity extends BaseActivity {
     @Override
     protected void onCreate() {
         super.onCreate();
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+        initSlidingLayout();
+    }
+
+    private void initSlidingLayout() {
+        sliding_layout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+                rel_complete_order.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
+                if (newState == SlidingUpPanelLayout.PanelState.EXPANDED) {
+                    rel_complete_order.setVisibility(View.GONE);
+                } else if (newState == SlidingUpPanelLayout.PanelState.COLLAPSED) {
+                    rel_complete_order.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        sliding_layout.setFadeOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sliding_layout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+            }
+        });
     }
 }
